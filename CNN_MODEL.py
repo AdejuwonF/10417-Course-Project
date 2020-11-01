e517dbc453aab3ef469a48ecf78dd7da7037d2e7
afasanya <afasanya@andrew.cmu.edu>
2020-10-31 20:48:01 -0400

    Rewrote file paths

--- CNN_MODEL.py
+++ CNN_MODEL.py
@@ -10,8 +10,8 @@ import PIL
 import skimage.io as io
 
 
-coco_val = dset.CocoDetection(root = '/Users/adejuwon/Desktop/CMU School Junk/Fall 2020/10417 Intermediate Deep Learning/Course Project/COCO_DATASET/val2017',
-                        annFile = '/Users/adejuwon/Desktop/CMU School Junk/Fall 2020/10417 Intermediate Deep Learning/Course Project/COCO_DATASET/annotations/instances_val2017.json',
+coco_val = dset.CocoDetection(root = './COCO_DATASET/val2017',
+                        annFile = './COCO_DATASET/annotations/instances_val2017.json',
                         transform=transforms.ToTensor())
 
 print('Number of samples: ', len(coco_val))
@@ -22,7 +22,7 @@ target_id = target['category_id']
 print("Image Size: ", img.size())
 print(target)
 
-coco = COCO(annotation_file="/Users/adejuwon/Desktop/CMU School Junk/Fall 2020/10417 Intermediate Deep Learning/Course Project/COCO_DATASET/annotations/instances_val2017.json")
+coco = COCO(annotation_file="./COCO_DATASET/annotations/instances_val2017.json")
 
 target_mask = coco.annToMask(target)
 cat_seg = coco.loadCats([target_id])
