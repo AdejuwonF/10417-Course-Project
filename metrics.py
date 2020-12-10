c142116f5a4c7ccbfe726d4fd00f3e24b5292985
afasanya <adejuwonfasanya@gmail.com>
2020-12-09 20:03:39 -0500

    Added trained classifier for inception score

--- metrics.py
+++ metrics.py
@@ -39,9 +39,11 @@ def inception_score(generator, classifier,  noise_dim, n_classes=10, num_samples
     return torch.exp(KL_div(posteriors, priors))
 
 wgan = WGAN()
-wgan.load("/Users/adejuwon/Desktop/CMU School Junk/Fall 2020/10417 Intermediate Deep Learning/Course Project/WGAN_WC_01/OLD_WGAN_0005/gan_wc_100_epochs2020_11_30_05:27:10")
+#wgan.load("/Users/adejuwon/Desktop/CMU School Junk/Fall 2020/10417 Intermediate Deep Learning/Course Project/WGAN_WC_01/OLD_WGAN_0005/gan_wc_100_epochs2020_11_30_05:27:10")
+#state_dict = torch.load("/Users/adejuwon/Desktop/CMU School Junk/Fall 2020/10417 Intermediate Deep Learning/Course Project/classifier_checkpoints/epoch50")
 
-state_dict = torch.load("/Users/adejuwon/Desktop/CMU School Junk/Fall 2020/10417 Intermediate Deep Learning/Course Project/classifier_checkpoints/epoch50")
-# state_dict = torch.load("/Users/adejuwon/Desktop/CMU School Junk/Fall 2020/10417 Intermediate Deep Learning/Course Project/classifier_checkpoints/classifier_checkpoints_ndf32/epoch10")
+state_dict = torch.load("classifier_checkpoints/classifier_checkpoints_ndf32/epoch10")
 classifier = state_dict["model"]
-print(inception_score(wgan.generator, classifier, 100, 10, 100))
+
+#gan = whatever the fuck
+#print(inception_score(gan.generator, classifier, 100, 10, 50000))
