51b16739e503efdfa16bee6071d427941695f0ca
AdejuwonF <44853827+AdejuwonF@users.noreply.github.com>
2020-12-10 22:47:31 -0500

    Update testing.py

--- GAN STUFF/testing.py
+++ GAN STUFF/testing.py
@@ -17,7 +17,7 @@ batch_size = models.batch_size
 workers = models.workers
 
 
-dataroot = "/Users/adejuwon/Desktop/Pet Projects/PokeGan/pokemon"
+dataroot = "pokemon"
 
 """dataset = dset.ImageFolder(root=dataroot,
                            transform=transforms.Compose([
@@ -57,7 +57,6 @@ dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
 #     plt.imshow(netG.forward(torch.randn((1, 100, 1, 1))).view(3,32,32).permute(1,2,0)*.5 + .5)
 
 model = models.WGAN()
-model.load("/Users/adejuwon/Desktop/Pet Projects/PokeGan/model_checkpoints/poke_gan_wc_1800_epochs_2020_11_14_05:38:08")
 #model.train(500, dataloader)
 #model = models.WGAN_GP()
 #model.train(60, dataloader)
