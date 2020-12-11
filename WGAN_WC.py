8c3da92ea58b2514e657e932e399e9834cfb702c
AdejuwonF <44853827+AdejuwonF@users.noreply.github.com>
2020-12-10 22:53:35 -0500

    Update WGAN_WC.py

--- WGAN_WC.py
+++ WGAN_WC.py
@@ -163,7 +163,7 @@ class WGAN(object):
 
     def save(self, fp=None):
         if fp is None:
-            fp = "/Users/adejuwon/Desktop/Pet Projects/PokeGan/model_checkpoints/poke_gan_wc_{0}_epochs_{1}"\
+            fp = "model_checkpoints/poke_gan_wc_{0}_epochs_{1}"\
                 .format(self.epochs, datetime.now().strftime("%Y_%m_%d_%H:%M:%S"))
         else:
             fp += "/gan_wc_{0}_epochs{1}".format(self.epochs, datetime.now().strftime("%Y_%m_%d_%H:%M:%S"))
